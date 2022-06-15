a4f3ef4c01f6ccfa73044d890aad3cc607336c62
Ravi <ravi@gmail.com>
2022-06-15 16:20:42 +0530

    print demo

---
+++ 3-variable.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+#declare variable
+a=100
+
+#print data in variable
+echo $a
+#or
+echo ${a}
+
+#arthematic substitution
+data= $((3+5))
+echo $data
+
+#command substitution
+DATE = $(date +%F)
+echo "today date is $DATE"
