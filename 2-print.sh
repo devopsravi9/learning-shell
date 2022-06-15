0f1013d51156740364c916c555578d3da3956c4b
Ravi <ravi@gmail.com>
2022-06-15 14:43:30 +0530

    print demo

--- 2-print.sh
+++ 2-print.sh
@@ -7,6 +7,11 @@
 echo hello world
 
 # to print new lines
+# syntax : echo -e "line1 \n line2"
 echo -e "hello world. \nhow is the day"
-
 # here -e enables backslash interpretation inside quotes.
+
+#print tab space using \t
+echo -e "hii \thello
+
+"
