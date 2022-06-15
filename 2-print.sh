e8c4307e6ff7e05d868c4d1a8d129e896d230f6a
Ravi <ravi@gmail.com>
2022-06-15 14:38:59 +0530

    test

---
+++ 2-print.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+# to print message
+#command : echo
+#sytax : echo INPUT
+
+echo hello world
+
+# to print new lines
+echo -e "hello world. \nhow is the day"
+
+# here -e enables backslash interpretation inside quotes.
