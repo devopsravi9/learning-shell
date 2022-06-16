593c6767ef6d4e7ef417d49d489f61c42ad9452d
Ravi <ravi@gmail.com>
2022-06-16 11:40:47 +0530

    spe var demo

--- 4-read.sh
+++ 4-read.sh
@@ -4,4 +4,4 @@ read -p 'enter your name:' name
 echo -e "hey $name welcome"
 
 read -p "enter your number" phn
-echo -p "we sen details to your $phn"
+echo -e "we sen details to your $phn"
---
+++ 5-spec var.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+echo "script name" = $0
+echo "first input =" $1
+echo "second input =" $2
+echo "all inputs =" $@
+echo "no. of inputs =" $#
