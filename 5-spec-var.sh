e48de6fc31482c80dd7c1f6839ec36bdb98efdb1
Ravi <ravi@gmail.com>
2022-06-16 12:04:23 +0530

    spe var demo

--- 5-spec-var.sh
+++ 5-spec-var.sh
@@ -5,3 +5,6 @@ echo "first input =" $1
 echo "second input =" $2
 echo "all inputs =" $@
 echo "no. of inputs =" $#
+
+#in shell give bash <script name> <first i/p> <second i/p>
+# from after 10 arguement we use ${10} instead $10
