a8f19be58a39c133b22a9ebb07184b28ba4d9516
Ravi <ravi@gmail.com>
2022-07-04 19:41:16 +0530

    ec2 code

--- 1-bash-sing.sh
+++ 1-bash-sing.sh
@@ -1,28 +1,23 @@
-# #!/usr/bin/bash
+#!/usr/bin/bash
+
+ls
+cat /etc/passwd
+
+# PUBLIC_IP=$(aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}')
 #
-# ls
-# cat /etc/passwd
-
-PUBLIC_IP=$(aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}')
-#aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}'
-
-export PUBLIC_IP
-
- echo '{
-  "Comment": "CREATE/DELETE/UPSERT a record ",
-  "Changes": [{
-    "Action": "UPSERT",
-    "ResourceRecordSet": {
-      "Name": "jenkins.devopsravi.online",
-      "Type": "A",
-      "TTL": 30,
-      "ResourceRecords": [{ "Value": "IPADDRESS"}]
-    }}]
-}' | sed -i -e 's/IPADDRESS/$PUBLIC_IP/' >/tmp/record.json
-
-cat /tmp/record.json
-
-aws route53 change-resource-record-sets --hosted-zone-id Z0462442QH5T6H1KPDGO --change-batch file:///tmp/record.json
-
+#  echo '{
+#   "Comment": "CREATE/DELETE/UPSERT a record ",
+#   "Changes": [{
+#     "Action": "UPSERT",
+#     "ResourceRecordSet": {
+#       "Name": "jenkins.devopsravi.online",
+#       "Type": "A",
+#       "TTL": 30,
+#       "ResourceRecords": [{ "Value": "IPADDRESS"}]
+#     }}]
+# }' | sed -e "s/IPADDRESS/$PUBLIC_IP/" >/tmp/record.json
+#
+# aws route53 change-resource-record-sets --hosted-zone-id Z0462442QH5T6H1KPDGO --change-batch file:///tmp/record.json
+#
 
 
