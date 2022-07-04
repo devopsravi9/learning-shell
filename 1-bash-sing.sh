9102e7c77a704275c5e2301a35e571d3087d974f
Ravi <ravi@gmail.com>
2022-07-04 16:55:35 +0530

    ec2 code

--- 1-bash-sing.sh
+++ 1-bash-sing.sh
@@ -4,7 +4,10 @@
 # cat /etc/passwd
 
 PUBLIC_IP=$(aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}')
-aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}'
+#aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1 --output table | grep PublicIpAddress | awk '{print$4}'
+
+echo $PUBLIC_IP
+
  echo '{
   "Comment": "CREATE/DELETE/UPSERT a record ",
   "Changes": [{
@@ -18,5 +21,5 @@ aws ec2 describe-instances --instance-id=i-01fccf0b9d2010e10  --region=us-east-1
 }' | sed -e 's/IPADDRESS/$PUBLIC_IP/' >/tmp/record.json
 cat /tmp/record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z0462442QH5T6H1KPDGO --change-batch file:///tmp/record.json
-echo $PUBLIC_IP
+
 
