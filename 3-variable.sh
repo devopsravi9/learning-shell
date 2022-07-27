4ec6ceab3fba78f39ab4c3002f6f39cfc8466e45
Ravi <ravi@gmail.com>
2022-07-27 15:21:34 +0530

    ec2 code

--- 3-variable.sh
+++ 3-variable.sh
@@ -16,3 +16,7 @@ echo $data
 #command substitution
 DATE=$(date +%F)
 echo today date is $DATE
+
+# shell has its own pid  & own memory to store vars. shell called parent
+# script also has its own pid  & own memory to store vars. script called child.
+# to use shell vars in script we should mage parent-child relation using export command on shell
