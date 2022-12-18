1ccbabd3d87236f4a52933183d0ee9b0dad6fb2c
devopsravi10 <120667453+devopsravi10@users.noreply.github.com>
2022-12-18 21:37:24 +0530

    Update 1-bash-sing.sh

--- 1-bash-sing.sh
+++ 1-bash-sing.sh
@@ -18,6 +18,7 @@ cat /etc/passwd
 # }' | sed -e "s/IPADDRESS/$PUBLIC_IP/" >/tmp/record.json
 #
 # aws route53 change-resource-record-sets --hosted-zone-id Z0462442QH5T6H1KPDGO --change-batch file:///tmp/record.json
-#
+#add test lines
+
 
 
